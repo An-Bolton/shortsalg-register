f72a4d3315445b3836e541030865b0f49d38bc47
bolton <andreas.seielstad@gmail.com>
2026-08-16 13:02:05 +0200

    Update app text

--- shortsalg_app.py
+++ shortsalg_app.py
@@ -1188,8 +1188,7 @@ with tab_about:
     st.header("Om plattformen")
     st.markdown(
         """
-        Shortsalg-registeret er en uavhengig og uoffisiell analysetjeneste. Tjenesten er ikke utviklet, godkjent eller drevet av Finanstilsynet. Data hentes fra Finanstilsynets offentlige Short Sale Register (SSR)
-        ,og det var dette utgangspunktet som jeg bygget shortsregisteret videre på ved Universitetet i Oxford - Saïd Business School som en del av et innleveringsprosjekt. Den er utviklet for læring, markedsinnsikt og enklere tilgang til
+        Shortsalg-registeret er en uavhengig og uoffisiell analysetjeneste. Tjenesten er ikke utviklet, godkjent eller drevet av Finanstilsynet. Data hentes fra Finanstilsynets offentlige Short Sale Register (SSR), og det var dette utgangspunktet som jeg bygget shortsregisteret videre på ved Universitetet i Oxford - Saïd Business School som en del av et innleveringsprosjekt. Den er utviklet for læring, markedsinnsikt og enklere tilgang til
         historiske data.
 
         **Teknologi:** Python, Streamlit, Pandas, Plotly og SQLite. API fra Finanstilsynet: https://ssr.finanstilsynet.no/api/v2/
